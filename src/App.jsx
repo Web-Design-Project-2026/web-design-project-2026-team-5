44dcd6fdbd9423e5b8e62be18777c082ad0dfb10
quanhoangindex <quanhoangindex@gmail.com>
2026-04-26 23:09:00 +0200

    add: basic pages, components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Platform from "./pages/Platform";
+import Contact from "./pages/Platform";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
-            <h1>Hello Let's go</h1>
-        </>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/platform" element={<Platform />}></Route>
+                <Route path="/Contact" element={<Contact />}></Route>
+                <Route path="/About" element={<About />}></Route>
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     );
 }
 
