c7e8ffb7b58dd70239bbfc024b5eadfd93abb38f
quanhoangindex <quanhoangindex@gmail.com>
2026-04-27 15:47:23 +0200

    add: icon to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import NavLink from "./ui/NavLink";
+import { ArrowUpRight } from "lucide-react";
 
 function Navbar() {
     return (
         <nav>
-            <NavLink href="/">Home</NavLink>
+            <NavLink href="/">
+                Home <ArrowUpRight size={16} />
+            </NavLink>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
