36ba27326c9e7ac067b364bc2a6a4e753ff325d1
quanhoangindex <quanhoangindex@gmail.com>
2026-04-27 18:48:11 +0200

    add: full header function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,27 +3,28 @@ import { Link } from "react-router-dom";
 import { ArrowUpRight } from "lucide-react";
 import Button from "./ui/Button";
 import Logo from "../assets/logoInnoBrain.svg";
+import "./Header.css";
 
 function Header() {
     return (
-        <header>
-            <div>
-                <Link to="/">
+        <header className="header">
+            <div className="header__inner">
+                <Link to="/" className="header__logo">
                     <img src={Logo} alt="InnoBrain" />
                 </Link>
-            </div>
 
-            <nav>
-                <NavLink to="/platform">Platform</NavLink>
-                <NavLink to="#">Industries</NavLink>
-                <NavLink to="/about">About Us</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-                <NavLink to="#">Sector</NavLink>
-            </nav>
+                <nav className="header__nav">
+                    <NavLink to="/platform">Platform</NavLink>
+                    <NavLink to="#">Industries</NavLink>
+                    <NavLink to="/about">About Us</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="#">Sector</NavLink>
+                </nav>
 
-            <div className="header__actions">
-                <Button variant="outline">Login</Button>
-                <Button>Book a Free Demo</Button>
+                <div className="header__actions">
+                    <Button variant="outline">Login</Button>
+                    <Button>Book a Free Demo</Button>
+                </div>
             </div>
         </header>
     );
