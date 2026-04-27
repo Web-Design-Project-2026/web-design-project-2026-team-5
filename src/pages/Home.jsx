7936bef7bd8975a5114b5edb1048407cce2467b6
quanhoangindex <quanhoangindex@gmail.com>
2026-04-27 23:39:32 +0200

    feat: content, logo render with alt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import HomeHero from "../components/sections/HomeHero";
+import HomeTrustpartner from "../components/sections/HomeTrustPartner";
 function Home() {
     return (
         <main>
             <HomeHero></HomeHero>
+            <HomeTrustpartner/>
         </main>
     );
 }
